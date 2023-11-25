abdbc852d301ae578cd0cd1a22c2e77dbe3410f5
FRCroboSam <samuelwang787@gmail.com>
2023-11-25 05:43:08 -0800

    Made photos work with sanity

--- app/components/PhotoCard.js
+++ app/components/PhotoCard.js
@@ -2,19 +2,19 @@
 import { urlForImage } from "@/sanity/lib/image";
 import Image from "next/image";
 
-export default function Photo({ photo: { title, image, favorite } }) {
+export default function Photo({ photo }) {
   return (
     <div className="space-y-2 group">
       <Image
-        src={urlForImage(image).auto("format").size(1920, 1080).url()}
+        src={urlForImage(photo.image).auto("format").size(1920, 1080).url()}
         width={1920}
         height={1080}
-        alt={title}
+        alt={photo.description}
         className="rounded-2xl border border-primary-400 md:group-hover:scale-95 transition-transform transform"
       />
       <h2 className="font-medium flex items-center justify-center">
-        {favorite ? <p className="text-sm mr-2">⭐️</p> : null}
-        {title}
+        {photo.favorite ? <p className="text-sm mr-2">⭐️</p> : null}
+        {photo.description}
       </h2>
     </div>
   );
